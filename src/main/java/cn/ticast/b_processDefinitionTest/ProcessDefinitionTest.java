c01e2c31ad494395836b1d495226197532e66db4
gaohanghang <1341947277@qq.com>
2018-03-21 18:15:12 +0800

    更新

--- src/main/java/cn/ticast/b_processDefinitionTest/ProcessDefinitionTest.java
+++ src/main/java/cn/ticast/b_processDefinitionTest/ProcessDefinitionTest.java
@@ -3,9 +3,12 @@ package cn.ticast.b_processDefinitionTest;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.security.PublicKey;
+import java.util.List;
 import java.util.zip.ZipInputStream;
 
 public class ProcessDefinitionTest {
@@ -46,7 +49,69 @@ public class ProcessDefinitionTest {
     /**
      * 查询流程定义
      */
+    @Test
     public void findProcessDefinition() {
-        processEngine.getRepositoryService();//与流程定义相关
+        List<ProcessDefinition> list = processEngine.getRepositoryService()//与流程定义和部署对象相关的Service
+                .createProcessDefinitionQuery()//创建一个流程定义的查询
+                /**指定查询条件，where条件*/
+//                        .deploymentId(deploymentId)//使用部署对象ID查询
+//                        .processDefinitionId(processDefinitionId)//使用流程定义ID查询
+//                        .processDefinitionKey(processDefinitionKey)//使用流程定义的key查询
+//                        .processDefinitionNameLike(processDefinitionNameLike)//使用流程定义的名称模糊查询
+
+                /**排序*/
+                .orderByProcessDefinitionVersion().asc()//按照版本的升序排列
+//                        .orderByProcessDefinitionName().desc()//按照流程定义的名称降序排列
+
+                /**返回的结果集*/
+                .list();//返回一个集合列表，封装流程定义
+//                        .singleResult();//返回唯一结果集
+//                        .count();//返回唯一结果集
+//                        .listPage(firstResult, maxResults);//分页查询
+
+            if (list!=null && list.size()>0) {
+                for (ProcessDefinition pd : list) {
+                    System.out.println("流程定义ID:"+pd.getId());//流程定义的key+版本+随机数
+                    System.out.println("流程定义的名称:"+pd.getName());//对应helloworld.bpmn文件中的name属性值
+                    System.out.println("流程定义的Key:"+pd.getKey());//对应helloworld.bpmn文件中的id属性值
+                    System.out.println("流程定义的版本:"+pd.getVersion());//当流程定义的key值相同的情况下，版本升级，默认1
+                    System.out.println("资源名称bpmn文件:"+pd.getResourceName());
+                    System.out.println("资源名称png文件:"+pd.getDiagramResourceName());
+                    System.out.println("部署对象ID: "+pd.getDeploymentId());
+                    System.out.println("##################################");
+                }
+            }
+
+    }
+
+    /**
+     * 删除流程定义
+     */
+    @Test
+    public void deleteProcessDefinition(){
+        //使用部署ID，完成删除
+        String deploymentId = "701";
+        /**
+         * 不带级联的删除
+         *      只能删除没有启动的流程，如果流程启动，就会抛出异常
+         */
+//        processEngine.getRepositoryService()//
+//                        .deleteDeployment(deploymentId);
+
+        /**
+         * 级联删除
+         *  不管流程是否启动，都可以删除
+         */
+        processEngine.getRepositoryService()//
+                        .deleteDeployment(deploymentId, true);
+        System.out.println("删除成功！");
+    }
+
+    /**
+     * 查看流程图
+     */
+    @Test
+    public void viewPic() {
+
     }
 }
