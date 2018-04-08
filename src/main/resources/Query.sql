97f984f6b7976da75193b18b1da02be27fec0ce9
gaohanghang <1341947277@qq.com>
2018-04-08 18:44:54 +0800

    学习代码

--- src/main/resources/Query.sql
+++ src/main/resources/Query.sql
@@ -10,7 +10,7 @@ SELECT * FROM act_ge_property			#主键生成策略
 #流程实例, 执行对象, 任务
 SELECT * FROM act_ru_execution  #正在执行的执行对象表
 
-SELECT * FROM ect_hi_proinst  #流程实例的历史表
+SELECT * FROM act_hi_procinst  #流程实例的历史表
 
 SELECT * FROM act_ru_task     #正在执行的任务表（只有节点是UserTask的时候，该表中存在数据）
 
@@ -24,3 +24,8 @@ SELECT * FROM act_hi_actinst  #所有活动节点的历史表
 SELECT * FROM act_ru_variable #正在执行的流程变量表
 
 SELECT * FROM act_hi_varinst #历史的流程变量表
+
+##########################################
+SELECT * FROM act_ru_identitylink  #任务表(个人任务，组任务)
+
+SELECT* FROM act_hi_identitylink #历史任务办理人表（个人任务，组任务）
